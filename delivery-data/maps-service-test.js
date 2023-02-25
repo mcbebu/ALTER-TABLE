7ebb0b9b45b9ce210e6f047e48664e9d2c8d80f2
Petchie <53936910+PatrickPetch@users.noreply.github.com>
2023-02-25 16:06:54 +0800

    update map file

--- delivery-data/maps-service-test.js
+++ delivery-data/maps-service-test.js
@@ -1,10 +1,14 @@
 import axios from 'axios'
 
 let key = "AIzaSyDg64WEaXKx-ZOBBg_oklSYxpbP3koFp90";
-let origin = "NTU Hall of Residence 9".replace([" ", ","], ["%20", "%2C"]);
+let origin = "83 Genting Ln, Singapore 349568".replace([" ", ","], ["%20", "%2C"]);
 let desination = "NTU Hall of Residence 5".replace([" ", ","], ["%20", "%2C"]);
 
 
-let response = await axios.get(`https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${desination}&units=SI&key=${key}`);
+let response = await axios.get(
+    `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${origin}&destinations=${desination}&units=SI&key=${key}`
+);
+
 console.log(response.data);
+console.log(JSON.stringify(response.data.rows));
 
