183f742422735d1feb83d2e5a690c2b9b77898eb
NattapolChan <66078943+NattapolChan@users.noreply.github.com>
2023-02-25 12:59:53 +0800

    [Shipper] UI (shipper app)

---
+++ shipper-dashboard/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '@chakra-ui/react';
+
+const Navbar = () => {
+    return (
+        <Container w='100%' h='2em' bg='red.300' m='0'>
+            Navbar
+        </Container>
+    )
+}
+
+export default Navbar;
